136e4919ef7af0bac7b24b66824ca7b9dd75ebbc
Kevin DC <kevindelcastillo@ravn.co>
2023-03-22 10:53:37 -0500

    feat: Add `minQuantity` and `maxQuantity` to `Product` model

--- prisma/migrations/20230322154925_init/migration.sql
+++ prisma/migrations/20230322154925_init/migration.sql
@@ -35,6 +35,8 @@ CREATE TABLE "products" (
     "price" DECIMAL(10,2) NOT NULL,
     "thumbnail_url" TEXT,
     "state" "product_state" NOT NULL DEFAULT 'inactive',
+    "minQuantity" INTEGER NOT NULL DEFAULT 1,
+    "maxQuantity" INTEGER NOT NULL DEFAULT 1,
     "category_name" TEXT NOT NULL,
     "created_by_id" UUID NOT NULL,
 
