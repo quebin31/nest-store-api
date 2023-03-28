efb1d5166d551ce33ae7ef7ece942e4650b8cfa5
Kevin DC <kevindelcastillo@ravn.co>
2023-03-28 11:30:46 -0500

    feat: Improve `RolesGuard` to check if an endpoint is public

--- src/features/auth/roles.guard.ts
+++ src/features/auth/roles.guard.ts
@@ -1,9 +1,10 @@
 import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
-import { AuthRequest } from './jwt.strategy';
+import { OptionalAuthRequest } from './jwt.strategy';
 import { UsersRepository } from '../users/users.repository';
 import { Reflector } from '@nestjs/core';
 import { RolesKey } from '../../decorators/roles';
 import { Role } from '@prisma/client';
+import { PublicOptions, PublicOptionsKey } from '../../decorators/public';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
@@ -14,12 +15,20 @@ export class RolesGuard implements CanActivate {
   }
 
   async canActivate(context: ExecutionContext) {
-    const targets = [context.getClass(), context.getHandler()]
+    const targets = [context.getClass(), context.getHandler()];
     const roles = this.reflector.getAllAndMerge<Role[]>(RolesKey, targets);
-    if (!roles) return true;
+    if (!roles) {
+      return true;
+    }
 
-    const request = context.switchToHttp().getRequest<AuthRequest>();
-    const userId = request.user.id;
+    const request = context.switchToHttp().getRequest<OptionalAuthRequest>();
+    const userId = request.user?.id;
+    if (!userId) {
+      const publicOptions = this.reflector
+        .getAllAndOverride<PublicOptions | undefined>(PublicOptionsKey, targets);
+      return publicOptions !== undefined;
+    }
+
     const user = await this.usersRepository.findById(userId);
     if (!user) {
       throw new ForbiddenException(`Invalid user ${userId}`);
